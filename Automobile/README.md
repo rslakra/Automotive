5fdbc4bf598df6a0da949af713a912e15bc15f81
Rohtash Lakra <rslakra@users.noreply.github.com>
2025-01-20 15:51:51 -0800

    merge local changes and added docker file

--- Automobile/README.md
+++ Automobile/README.md
@@ -1,4 +1,11 @@
-# SpringBootJSP
+# Automobile
+
+
+# Reference****
+
+[Docker Awesome Compose](https://github.com/docker/awesome-compose)
+[Backend Dockerfile](https://github.com/docker/awesome-compose/blob/master/react-java-mysql/backend/Dockerfile)
+[Helper Readme])https://github.com/docker/awesome-compose/blob/master/react-java-mysql/README.md
 
 # Authors
 
--- Automobile/runMaven.sh
+++ Automobile/runMaven.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 #Author: Rohtash Lakra
 clear
-#echo
-#JAVA_VERSION=11
-#export JAVA_HOME=$(/usr/libexec/java_home -v $JAVA_VERSION)
+#set -euo pipefail
+#IFS=$'\n\t'
+VERSION="0.0"
+# Build Version Function
+function buildVersion() {
+  GIT_COMMIT_COUNT=$(git rev-list HEAD --count)
+  if [ $? -ne 0 ]; then
+    VERSION="${VERSION}.1"
+  else
+    VERSION="${VERSION}.${GIT_COMMIT_COUNT}"
+  fi
+  SNAPSHOT="${SNAPSHOT:-$!}"
+  if [[ ! -z ${SNAPSHOT} ]]; then
+      VERSION="${VERSION}-SNAPSHOT"
+  fi
+
+  echo "${VERSION}";
+}
+
+echo
 echo "${JAVA_HOME}"
 echo
+SNAPSHOT_VERSION=$(buildVersion SNAPSHOT)
+RELEASE_VERSION=$(buildVersion)
+#java -jar target/account-service-$RELEASE_VERSION.jar
 #mvn clean package -DskipTests
-mvn clean spring-boot:run
-#./mvnw spring-boot:run
+mvn clean spring-boot:run -Drevision=$RELEASE_VERSION
+echo
